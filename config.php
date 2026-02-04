d0bbc0ae6dc25b4e34d92001c306c9ab37088928
RELIQ20 <nathaniellawas05@gmail.com>
2026-02-04 23:03:20 +0800

    change in js loginForm = document.getElementsByClassName('form-box');

--- config.php
+++ config.php
@@ -1,24 +1,13 @@
 <?php
 
 
-
 $host = "localhost";
-
 $user = "root";
-
 $password = "";
-
 $database = "booking_db";
-
-
-
 $conn = new mysqli($host, $user, $password, $database);
-
-
-
 if ($conn->connect_error) {
-
     die("Connection failed: " . $conn->connect_error);
-
 }
 
+?>
